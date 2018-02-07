167247aa355252ad19d2a1d9d9ffbc8582c24693
MyNET <megaco789@gmail.com>
2018-02-07 11:23:35 +0800

    simple

--- src/Sticks/SticksMain.php
+++ src/Sticks/SticksMain.php
@@ -1,3 +1,20 @@
 <?php
 
 namespace Sticks;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+class SticksMain extends PluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getServer()->getLogger()->info(TextFormat::YELLOW. "Sticks v1 by MyNET Enable..");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+}
